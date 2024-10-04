ea308d6cec02c974dc9914c694484de926ede80e
AdarhKumar <adarshnit420@gmail.com>
2024-10-04 10:33:21 +0530

    first commit

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://AdarshKumar:7903848803@cluster0.bpglqqv.mongodb.net/schoolfee', {
+    // await mongoose.connect('mongodb+srv://AdarshKumar:7903848803@cluster0.bpglqqv.mongodb.net/schoolfee', {
+    await mongoose.connect('mongodb+srv://principalnngsttc:732498@cluster0.biu54.mongodb.net/collegefee',{
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
